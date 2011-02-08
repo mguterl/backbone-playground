98730a393d286bb1c1428c7c1b9e3f51b8d86744
Michael Guterl <mguterl@gmail.com>
2011-02-07 21:37:11 -0500

    update application.js to use coffeescript compiled version

--- application.js
+++ application.js
@@ -1,90 +1,68 @@
-var SearchApp = function() {
-  var SearchModel = Backbone.Model.extend({
-
-  });
-
-  var SearchAreaTemplate = _.template(
-    "<a class='previous' href='#!/page/<%= parseInt(page) - 1 %>'>Previous page</a> " +
-      "<span class='page'>Page: <%= page %></span> " +
-      "<a class='next' href='#!/page/<%= parseInt(page) + 1 %>'>Next page</a>"
-  );
-
-  var SearchAreaView = Backbone.View.extend({
-    el: $('#search'),
-
-    initialize: function() {
-      this.render = _.bind(this.render, this);
-      this.model.bind('change:page', this.render);
-
-      this.layout = SearchAreaTemplate;
-
-      return this;
-    },
-
-    render: function() {
-      var content = this.layout(this.model.toJSON());
-
-      $(this.el)
-        .empty()
-        .html(content);
-
-      if (parseInt(this.model.get('page')) > 1) {
-        this.$(".previous").show();
+(function() {
+  var SearchApp;
+  SearchApp = function() {
+    var SearchAreaTemplate, SearchAreaView, SearchCollection, SearchController, SearchModel;
+    SearchModel = Backbone.Model.extend({});
+    SearchAreaTemplate = _.template("<a class='previous' href='#!/page/<%= parseInt(page) - 1 %>'>Previous page</a> " + "<span class='page'>Page: <%= page %></span> " + "<a class='next' href='#!/page/<%= parseInt(page) + 1 %>'>Next page</a>");
+    SearchAreaView = Backbone.View.extend({
+      el: $('#search'),
+      initialize: function() {
+        this.render = _.bind(this.render, this);
+        this.model.bind('change:page', this.render);
+        this.layout = SearchAreaTemplate;
+        return this;
+      },
+      render: function() {
+        var content;
+        content = this.layout(this.model.toJSON());
+        $(this.el).empty().html(content);
+        if (parseInt(this.model.get('page')) > 1) {
+          this.$(".previous").show();
+        }
+        return this;
       }
-
-      return this;
-    }
+    });
+    SearchCollection = Backbone.Collection.extend({
+      initialize: function(args) {
+        return this;
+      }
+    });
+    SearchController = Backbone.Controller.extend({
+      routes: {
+        "": "welcome",
+        "!/page/:pageNumber": "page"
+      },
+      initialize: function(options) {
+        this.instance = new SearchModel({
+          id: 1,
+          page: 1
+        });
+        this.SearchArea = new SearchAreaView({
+          model: this.instance
+        });
+        this.SearchArea.render();
+        return this;
+      },
+      welcome: function() {
+        return this;
+      },
+      page: function(pageNumber) {
+        this.instance.set({
+          page: pageNumber
+        });
+        return this;
+      }
+    });
+    return {
+      init: function() {
+        var Controller;
+        Controller = new SearchController();
+        Backbone.history.start();
+        return console.log('Started');
+      }
+    };
+  };
+  $(function() {
+    return SearchApp().init();
   });
-
-  var SearchCollection = Backbone.Collection.extend({
-    initialize: function(args) {
-      return this;
-    }
-  });
-
-  var SearchController = Backbone.Controller.extend({
-    routes: {
-      "": "welcome",
-      "!/page/:pageNumber" : "page"
-    },
-
-    initialize: function(options) {
-      this.instance = new SearchModel({
-        id: 1,
-        page: 1
-      });
-
-      this.SearchArea = new SearchAreaView({
-        model: this.instance
-      });
-
-      this.SearchArea.render();
-
-      return this;
-    },
-
-    welcome: function() {
-      return this;
-    },
-
-    page: function(pageNumber) {
-      this.instance.set({page: pageNumber});
-
-      return this;
-    }
-  });
-
-  return {
-    init: function() {
-	    var Controller = new SearchController();
-
-	    // Inform Backbone we're ready to start handling routes.
-	    Backbone.history.start();
-	    console.log('Started.');
-    }
-  }
-}();
-
-$(function(){
-  SearchApp.init();
-});
+}).call(this);
